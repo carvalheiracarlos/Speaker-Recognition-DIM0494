1583dec391d54e0def2f1b40bf91e157519f45e5
carvalheiracarlos <carvalheirafc@gmail.com>
2022-12-01 00:53:24 -0300

    more training and removed callbacks subfolders

--- models/sequential_conv2d_model.py
+++ models/sequential_conv2d_model.py
@@ -1,7 +1,11 @@
 from base.base_model import BaseModel
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.layers import Input, Dense, Conv2D, BatchNormalization, MaxPooling2D, Dropout, Flatten, Resizing, Input
+from keras.layers import (
+    Input, Dense, Conv2D, 
+    BatchNormalization, MaxPooling2D, Dropout, 
+    Flatten, Resizing, Input, BatchNormalization
+)
 from keras.metrics import categorical_accuracy
 
 
@@ -10,20 +14,34 @@ class SpeakerConv2D(BaseModel):
         super(SpeakerConv2D, self).__init__(config)
         self.input_shape = input_shape
         self.n_labels = n_labels
-        self.optimizer = Adam()
+        self.optimizer = Adam(learning_rate=self.config.model.learning_rate, 
+                              beta_1=0.9, 
+                              beta_2=0.999, 
+                              epsilon=1e-08, 
+                              decay=self.config.model.learning_rate/ self.config.trainer.num_epochs
+                            )
         self.build_model()
 
     def build_model(self):
-
         self.model = Sequential()
+
         self.model.add(Input(shape=self.input_shape))
-        self.model.add(Resizing(32, 32)) 
-        self.model.add(Conv2D(32, 3, activation='relu')) 
+        self.model.add(Resizing(64, 69)) 
+
+        self.model.add(Conv2D(64, kernel_size=(8,8), strides=(2,2), activation='relu')) 
         self.model.add(MaxPooling2D())
-        self.model.add(Dropout(0.25))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+
+        self.model.add(Conv2D(128, kernel_size=(4,4), strides=(2, 2), activation='relu')) 
+        self.model.add(MaxPooling2D())
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+
+
         self.model.add(Flatten())
+
         self.model.add(Dense(128, activation='relu'))
-        self.model.add(Dropout(0.25))
         self.model.add(Dense(self.n_labels[1], activation='softmax'))
 
         self.model.compile(
